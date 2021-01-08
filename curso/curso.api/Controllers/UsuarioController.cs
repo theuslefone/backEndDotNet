7850b362f8ed390ac43851dbe37fcd39f15fc3bb
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-08 20:11:01 -0300

    Adiciona config na rota logar

--- curso/curso.api/Controllers/UsuarioController.cs
+++ curso/curso.api/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using curso.api.Models;
+﻿using curso.api.Filters;
+using curso.api.Models;
 using curso.api.Models.Usuario;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,12 +25,13 @@ namespace curso.api.Controllers
         [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewmodel))]
         [HttpPost]
         [Route("logar")]
+        [ValidacaoModelStateCustomizada]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(new ValidaCampoViewModelInput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
-            }
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(new ValidaCampoViewModelInput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+            //}
             return Ok(loginViewModelInput);
         }
 
