86ef75d829dfe02c8604e0851496ffb6000849ee
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-08 20:08:31 -0300

    Adiciona Filtro

--- curso/curso.api/Filters/ValidacaoModelStateCustomizada.cs
+++ curso/curso.api/Filters/ValidacaoModelStateCustomizada.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using curso.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace curso.api.Filters
 {
-    public class ValidacaoModelStateCustomizada
+    public class ValidacaoModelStateCustomizada : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var validaCampoViewModel = new ValidaCampoViewModelInput(context.ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage));
+                context.Result = new BadRequestObjectResult(validaCampoViewModel);
+            }
+        }
     }
 }
