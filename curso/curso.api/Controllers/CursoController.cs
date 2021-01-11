20d95d2cc5c8ed51a94bfa02436c453451778a2b
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-11 10:44:36 -0300

    Adiciona model curso e configura  método http

---
+++ curso/curso.api/Controllers/CursoController.cs
@@ -0,0 +1,26 @@
+﻿using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var CodigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+        
+    }
+}
