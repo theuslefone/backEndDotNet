aaddc64be19493b50f0ad0b2223287d97a2e0abf
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-12 14:38:57 -0300

    Cria CursosDbContext

---
+++ curso/curso.api/Infraestruture/data/CursosDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Infraestruture.data
+{
+    public class CursosDbContext
+    {
+    }
+}
