d432f0feb2911128702de1901566715eac0f62ee
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-12 14:35:20 -0300

    modifica Curso.cs

--- curso/curso.api/Bussines/Entites/Curso.cs
+++ curso/curso.api/Bussines/Entites/Curso.cs
@@ -7,6 +7,11 @@ namespace curso.api.Bussines.Entites
 {
     public class Curso
     {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public int CodigoUsuario { get; set; }
+        public virtual Usuario usuario { get; set; }
 
     }
 }
