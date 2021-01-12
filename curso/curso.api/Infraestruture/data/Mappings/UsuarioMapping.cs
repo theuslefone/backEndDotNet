d7a5fbe32d5c149e3e745e240cbe4a80af7dda18
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-12 14:56:45 -0300

    Adiciona TB_USUARIO

---
+++ curso/curso.api/Infraestruture/data/Mappings/UsuarioMapping.cs
@@ -0,0 +1,22 @@
+﻿using curso.api.Bussines.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Infraestruture.data.Mappings
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+    {
+        void IEntityTypeConfiguration<Usuario>.Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("TB_USUARIO");
+
+        }
+    }
+}
