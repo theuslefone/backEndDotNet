ee2ef28876ef4f3e7c416ca0a2350e5c825e596e
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-12 14:47:29 -0300

    Adiciona CursoMapping e UsuarioMapping

--- curso/curso.api/Infraestruture/data/Mappings/CursoMapping.cs
+++ curso/curso.api/Infraestruture/data/Mappings/CursoMapping.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace curso.api.Infraestruture.data
+namespace curso.api.Infraestruture.data.Mappings
 {
-    public class CursosDbContext
+    public class CursoMapping
     {
     }
 }
