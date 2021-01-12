6fc93f5974b506d30972887f2553038fb70532d5
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-12 15:02:50 -0300

    Adiciona TB_CURSO e define chave primaria

--- curso/curso.api/Infraestruture/data/Mappings/CursoMapping.cs
+++ curso/curso.api/Infraestruture/data/Mappings/CursoMapping.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using curso.api.Bussines.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace curso.api.Infraestruture.data.Mappings
 {
-    public class CursoMapping
+    public class CursoMapping : IEntityTypeConfiguration<Curso>
     {
+        void IEntityTypeConfiguration<Curso>.Configure(EntityTypeBuilder<Curso> builder)
+        {
+            builder.ToTable("TB_CURSO");
+            builder.HasKey(p => p.Codigo)
+        }
     }
 }
