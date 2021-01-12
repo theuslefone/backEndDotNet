5f9a7c1af2f0196557cf6e18af88519210464fad
matheus keeven <matheuskeeven22@hotmail.com>
2021-01-12 15:00:08 -0300

    Adiciona o restante das chaves

--- curso/curso.api/Infraestruture/data/Mappings/UsuarioMapping.cs
+++ curso/curso.api/Infraestruture/data/Mappings/UsuarioMapping.cs
@@ -18,6 +18,9 @@ namespace curso.api.Infraestruture.data.Mappings
             builder.ToTable("TB_USUARIO");
             builder.HasKey(p => p.Codigo);
             builder.Property(p => p.Codigo).ValueGeneratedOnAdd();
+            builder.Property(p => p.Login);
+            builder.Property(p => p.Email);
+            builder.Property(p => p.Senha);
         }
     }
 }
